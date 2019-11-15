0e68e6d67650baecfc80e6fa7db6257a7c45a80d
Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>
2019-11-15 23:08:55 +0100

    Refine example.sh script and add manual for using the scripts
    Signed-off-by: Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ A node to let a robot drive to a certain tf
 
 `rosrun tf_to_movebase tf_to_movebase_node _action_topic:=/4/move_base _tf_name_target:=/4/rest _max_duration:=100 __name:=tf_to_movebase_node`
 
+## Common usage of scripts
+
+1. Run any mapping with Rviz visualization
+1. Run a `rostopic echo points` on the Rviz topic which publishes geometry_msgs/PointStamped (e.g. `rostopic echo clicked_point | tee /tmp/cliecked_points.txt`)
+1. Use the click_point tool to select certain goal locations for the robot
+1. Format the output via `format_point.sh /tmp/cliecked_points.txt`
+1. Adapt the `example.sh` script accordingly, to let the robot drive to every location, one after another
+
 ## Arguments
 
 * `action_topic`: Topic for sending the action
--- scripts/example.sh
+++ scripts/example.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
+rosrun tf static_transform_publisher -1.89 -2.3 0 0 0 0 amiro4/odom target/0 100 &
+rosrun tf static_transform_publisher -0.83 -5.19 0 0 0 0 amiro4/odom target/1 100 &
+rosrun tf static_transform_publisher 3.92690944672 -3.34068918228 0 0 0 0 amiro4/odom target/2 100 &
+rosrun tf static_transform_publisher 3.67792129517 -0.934426307678 0 0 0 0 amiro4/odom target/3 100 &
+rosrun tf static_transform_publisher 3.88256835938 1.44574069977 0 0 0 0 amiro4/odom target/4 100 &
+rosrun tf static_transform_publisher 1.75352859497 3.57395935059 0 0 0 0 amiro4/odom target/5 100 &
+rosrun tf static_transform_publisher -1.09067249298 3.43783569336 0 0 0 0 amiro4/odom target/6 100 &
+rosrun tf static_transform_publisher 1.15436983109 -0.621222496033 0 0 0 0 amiro4/odom target/7 100 &
 
-# Create a TF from any 
-TF_COMMON="$1" # eg amiro4/odom
-ROBOT_PREFIX="$2" # eg amiro
 
-rosrun tf static_transform_publisher -1.81 -0.34 0 0 0 0 ${TF_COMMON} target/0 100 &
-rosrun tf static_transform_publisher -1.89 -2.3 0 0 0 0 ${TF_COMMON} target/1 100 &
-rosrun tf static_transform_publisher -0.83 -5.19 0 0 0 0 ${TF_COMMON} target/2 100 &
-rosrun tf static_transform_publisher 3.92690944672 -3.34068918228 0 0 0 0 ${TF_COMMON} target/3 100 &
-rosrun tf static_transform_publisher 3.67792129517 -0.934426307678 0 0 0 0 ${TF_COMMON} target/4 100 &
-rosrun tf static_transform_publisher 3.88256835938 1.44574069977 0 0 0 0 ${TF_COMMON} target/5 100 &
-rosrun tf static_transform_publisher 1.75352859497 3.57395935059 0 0 0 0 ${TF_COMMON} target/6 100 &
-rosrun tf static_transform_publisher -1.09067249298 3.43783569336 0 0 0 0 ${TF_COMMON} target/7 100 &
-rosrun tf static_transform_publisher 1.15436983109 -0.621222496033 0 0 0 0 ${TF_COMMON} target/8 100 &
+rosrun tf static_transform_publisher -1.09067249298 3.43783569336 0 0 0 0 amiro5/odom target/0 100 &
+rosrun tf static_transform_publisher 1.75352859497 3.57395935059 0 0 0 0 amiro5/odom target/1 100 &
+rosrun tf static_transform_publisher 1.15436983109 -0.621222496033 0 0 0 0 amiro5/odom target/2 100 &
+rosrun tf static_transform_publisher -1.81 -0.34 0 0 0 0 amiro5/odom target/3 100 &
+rosrun tf static_transform_publisher -1.89 -2.3 0 0 0 0 amiro5/odom target/4 100 &
+rosrun tf static_transform_publisher -0.83 -5.19 0 0 0 0 amiro5/odom target/5 100 &
+rosrun tf static_transform_publisher 3.92690944672 -3.34068918228 0 0 0 0 amiro5/odom target/6 100 &
+rosrun tf static_transform_publisher 3.67792129517 -0.934426307678 0 0 0 0 amiro5/odom target/7 100 &
 
 function go() {
-rosrun tf_to_movebase tf_to_movebase_node _action_topic:=/${1}/move_base _tf_name_source:=${TF_COMMON} _tf_name_target:=/target/${2} _max_duration:=100 __name:=tf_to_movebase_${1}_${2}
+rosrun tf_to_movebase tf_to_movebase_node _action_topic:=/${1}/move_base _tf_name_source:=${3} _tf_name_target:=/target/${2} _max_duration:=100 __name:=tf_to_movebase_${1}_${2}
 }
 
-
 function go_4() {
-go ${ROBOT_PREFIX}4 1
-go ${ROBOT_PREFIX}4 2
-go ${ROBOT_PREFIX}4 3
-go ${ROBOT_PREFIX}4 4
-go ${ROBOT_PREFIX}4 5
-go ${ROBOT_PREFIX}4 6
-go ${ROBOT_PREFIX}4 7
+	for IDX in $(seq 0 7); do
+		go amiro4 $IDX amiro4/odom
+	done
 }
 
 function go_5() {
-go ${ROBOT_PREFIX}5 7
-go ${ROBOT_PREFIX}5 6
-go ${ROBOT_PREFIX}5 8
-go ${ROBOT_PREFIX}5 0
-go ${ROBOT_PREFIX}5 1
-go ${ROBOT_PREFIX}5 2
-go ${ROBOT_PREFIX}5 3
-go ${ROBOT_PREFIX}5 4
+	for IDX in $(seq 0 17); do
+		go amiro5 $IDX amiro5/odom
+	done
 }
 
 go_4 &
@@ -55,4 +49,5 @@ while rosnode list | grep -q tf_to_movebase; do
 done
 
 killall rosmaster
-
+killall static_transform_publisher
+killall tf_to_movebase_node
